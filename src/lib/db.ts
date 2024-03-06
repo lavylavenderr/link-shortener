078c87a77834f619e607283acf42e7bd612a7b3b
Lavender <alexander.hyman05@gmail.com>
2024-03-06 14:48:36 -0800

    feat: nvm no accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,9 @@
-import { withAccelerate } from '@prisma/extension-accelerate';
-import { PrismaClient } from "@prisma/client/edge"
+import { PrismaClient } from "@prisma/client"
 
-export const prisma = new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
